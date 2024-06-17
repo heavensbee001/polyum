84e0937be0760ec43e9be301c49e87f7bd299fdf
eloigil <eloi@joineden.ai>
2024-06-18 01:26:51 +0200

    Fix Navbar

--- src/components/layouts/TopNavBar.tsx
+++ src/components/layouts/TopNavBar.tsx
@@ -4,18 +4,21 @@ interface TopNavBarProps {}
 
 const TopNavBar: React.FC<TopNavBarProps> = ({}) => {
   return (
-    <nav className="h-16 mb-8">
-      <div className="h-full px-4 max-w-screen-lg mx-auto flex items-center">
-        <Image
-          src="/icons/polyum-icon.svg"
-          alt="Polyum Logo"
-          width={24}
-          height={24}
-          className="mr-2"
-        />
-        <span className="font-black text-3xl">Polyum</span>
-      </div>
-    </nav>
+    <>
+      <nav className="h-16 mb-8 border-b border-neutral-100 fixed top-0 left-0 w-screen bg-white z-30">
+        <div className="h-full px-4 max-w-screen-lg mx-auto flex items-center">
+          <Image
+            src="/icons/polyum-icon.svg"
+            alt="Polyum Logo"
+            width={24}
+            height={24}
+            className="mr-2"
+          />
+          <span className="font-black text-3xl">Polyum</span>
+        </div>
+      </nav>
+      <div className="h-16 w-screen mb-8"></div>
+    </>
   );
 };
 
